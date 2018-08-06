89ccc6e3f05b6a6c431790dbdd5b6ff260f5cc6d
edsilv <e.silverton@gmail.com>
2018-08-06 12:53:19 +0100

    add level

--- ServiceProfile.ts
+++ ServiceProfile.ts
@@ -1,18 +1,18 @@
 export enum ServiceProfile {
 
     // image api
-    IMAGE_COMPLIANCE_0 =                        "http://library.stanford.edu/iiif/image-api/compliance.html#level0",
-    IMAGE_COMPLIANCE_1 =                        "http://library.stanford.edu/iiif/image-api/compliance.html#level1",
-    IMAGE_COMPLIANCE_2 =                        "http://library.stanford.edu/iiif/image-api/compliance.html#level2",
-    IMAGE_CONFORMANCE_0 =                       "http://library.stanford.edu/iiif/image-api/conformance.html#level0",
-    IMAGE_CONFORMANCE_1 =                       "http://library.stanford.edu/iiif/image-api/conformance.html#level1",
-    IMAGE_CONFORMANCE_2 =                       "http://library.stanford.edu/iiif/image-api/conformance.html#level2",
-    IMAGE_1_COMPLIANCE_0 =                      "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level0",
-    IMAGE_1_COMPLIANCE_1 =                      "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level1",
-    IMAGE_1_COMPLIANCE_2 =                      "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level2",
-    IMAGE_1_CONFORMANCE_0 =                     "http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level0",
-    IMAGE_1_CONFORMANCE_1 =                     "http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level1",
-    IMAGE_1_CONFORMANCE_2 =                     "http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level2",
+    IMAGE_COMPLIANCE_LEVEL_0 =                  "http://library.stanford.edu/iiif/image-api/compliance.html#level0",
+    IMAGE_COMPLIANCE_LEVEL_1 =                  "http://library.stanford.edu/iiif/image-api/compliance.html#level1",
+    IMAGE_COMPLIANCE_LEVEL_2 =                  "http://library.stanford.edu/iiif/image-api/compliance.html#level2",
+    IMAGE_CONFORMANCE_LEVEL_0 =                 "http://library.stanford.edu/iiif/image-api/conformance.html#level0",
+    IMAGE_CONFORMANCE_LEVEL_1 =                 "http://library.stanford.edu/iiif/image-api/conformance.html#level1",
+    IMAGE_CONFORMANCE_LEVEL_2 =                 "http://library.stanford.edu/iiif/image-api/conformance.html#level2",
+    IMAGE_1_COMPLIANCE_LEVEL_0 =                "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level0",
+    IMAGE_1_COMPLIANCE_LEVEL_1 =                "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level1",
+    IMAGE_1_COMPLIANCE_LEVEL_2 =                "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level2",
+    IMAGE_1_CONFORMANCE_LEVEL_0 =               "http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level0",
+    IMAGE_1_CONFORMANCE_LEVEL_1 =               "http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level1",
+    IMAGE_1_CONFORMANCE_LEVEL_2 =               "http://library.stanford.edu/iiif/image-api/1.1/conformance.html#level2",
     IMAGE_1_LEVEL_0 =                           "http://iiif.io/api/image/1/level0.json",
     IMAGE_1_LEVEL_0_PROFILE =                   "http://iiif.io/api/image/1/profiles/level0.json",
     IMAGE_1_LEVEL_1 =                           "http://iiif.io/api/image/1/level1.json",
@@ -41,11 +41,10 @@ export enum ServiceProfile {
     AUTH_1_TOKEN =                              "http://iiif.io/api/auth/1/token",
 
     // search api
-    AUTO_COMPLETE =                             "http://iiif.io/api/search/0/autocomplete",
     SEARCH =                                    "http://iiif.io/api/search/0/search",
-
-    AUTO_COMPLETE_1 =                           "http://iiif.io/api/search/1/autocomplete",
+    SEARCH_AUTO_COMPLETE =                      "http://iiif.io/api/search/0/autocomplete",
     SEARCH_1 =                                  "http://iiif.io/api/search/1/search",
+    SEARCH_AUTO_COMPLETE_1 =                    "http://iiif.io/api/search/1/autocomplete",
 
     // extensions
     TRACKING_EXTENSIONS =                       "http://universalviewer.io/tracking-extensions-profile",
