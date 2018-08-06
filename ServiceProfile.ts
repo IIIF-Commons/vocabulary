243fec10201a373708d643190b2a089ef83d0cd6
edsilv <e.silverton@gmail.com>
2018-08-06 12:56:24 +0100

    add 0

--- ServiceProfile.ts
+++ ServiceProfile.ts
@@ -1,12 +1,12 @@
 export enum ServiceProfile {
 
     // image api
-    IMAGE_COMPLIANCE_LEVEL_0 =                  "http://library.stanford.edu/iiif/image-api/compliance.html#level0",
-    IMAGE_COMPLIANCE_LEVEL_1 =                  "http://library.stanford.edu/iiif/image-api/compliance.html#level1",
-    IMAGE_COMPLIANCE_LEVEL_2 =                  "http://library.stanford.edu/iiif/image-api/compliance.html#level2",
-    IMAGE_CONFORMANCE_LEVEL_0 =                 "http://library.stanford.edu/iiif/image-api/conformance.html#level0",
-    IMAGE_CONFORMANCE_LEVEL_1 =                 "http://library.stanford.edu/iiif/image-api/conformance.html#level1",
-    IMAGE_CONFORMANCE_LEVEL_2 =                 "http://library.stanford.edu/iiif/image-api/conformance.html#level2",
+    IMAGE_0_COMPLIANCE_LEVEL_0 =                "http://library.stanford.edu/iiif/image-api/compliance.html#level0",
+    IMAGE_0_COMPLIANCE_LEVEL_1 =                "http://library.stanford.edu/iiif/image-api/compliance.html#level1",
+    IMAGE_0_COMPLIANCE_LEVEL_2 =                "http://library.stanford.edu/iiif/image-api/compliance.html#level2",
+    IMAGE_0_CONFORMANCE_LEVEL_0 =               "http://library.stanford.edu/iiif/image-api/conformance.html#level0",
+    IMAGE_0_CONFORMANCE_LEVEL_1 =               "http://library.stanford.edu/iiif/image-api/conformance.html#level1",
+    IMAGE_0_CONFORMANCE_LEVEL_2 =               "http://library.stanford.edu/iiif/image-api/conformance.html#level2",
     IMAGE_1_COMPLIANCE_LEVEL_0 =                "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level0",
     IMAGE_1_COMPLIANCE_LEVEL_1 =                "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level1",
     IMAGE_1_COMPLIANCE_LEVEL_2 =                "http://library.stanford.edu/iiif/image-api/1.1/compliance.html#level2",
@@ -27,11 +27,11 @@ export enum ServiceProfile {
     IMAGE_2_LEVEL_2_PROFILE =                   "http://iiif.io/api/image/2/profiles/level2.json",
 
     // auth api
-    AUTH_CLICK_THROUGH =                        "http://iiif.io/api/auth/0/login/clickthrough",
-    AUTH_LOGIN =                                "http://iiif.io/api/auth/0/login",
-    AUTH_LOGOUT =                               "http://iiif.io/api/auth/0/logout",
-    AUTH_RESTRICTED =                           "http://iiif.io/api/auth/0/login/restricted",
-    AUTH_TOKEN =                                "http://iiif.io/api/auth/0/token",
+    AUTH_0_CLICK_THROUGH =                      "http://iiif.io/api/auth/0/login/clickthrough",
+    AUTH_0_LOGIN =                              "http://iiif.io/api/auth/0/login",
+    AUTH_0_LOGOUT =                             "http://iiif.io/api/auth/0/logout",
+    AUTH_0_RESTRICTED =                         "http://iiif.io/api/auth/0/login/restricted",
+    AUTH_0_TOKEN =                              "http://iiif.io/api/auth/0/token",
 
     AUTH_1_CLICK_THROUGH =                      "http://iiif.io/api/auth/1/clickthrough",
     AUTH_1_EXTERNAL =                           "http://iiif.io/api/auth/1/external",
@@ -41,10 +41,10 @@ export enum ServiceProfile {
     AUTH_1_TOKEN =                              "http://iiif.io/api/auth/1/token",
 
     // search api
-    SEARCH =                                    "http://iiif.io/api/search/0/search",
-    SEARCH_AUTO_COMPLETE =                      "http://iiif.io/api/search/0/autocomplete",
+    SEARCH_0 =                                  "http://iiif.io/api/search/0/search",
+    SEARCH_0_AUTO_COMPLETE =                    "http://iiif.io/api/search/0/autocomplete",
     SEARCH_1 =                                  "http://iiif.io/api/search/1/search",
-    SEARCH_AUTO_COMPLETE_1 =                    "http://iiif.io/api/search/1/autocomplete",
+    SEARCH_1_AUTO_COMPLETE =                    "http://iiif.io/api/search/1/autocomplete",
 
     // extensions
     TRACKING_EXTENSIONS =                       "http://universalviewer.io/tracking-extensions-profile",
