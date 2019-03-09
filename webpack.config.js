84ff408b8fee0228d2a2f4302acb5b6a7396f910
edsilv <e.silverton@gmail.com>
2019-03-09 17:26:20 +0000

    added dist-var bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 
-module.exports = {
+function resolvePath(p) {
+    return path.resolve(__dirname, p)
+}
+
+const config = {
     // These are the entry point of our library. We tell webpack to use the name we assign later, when creating the bundle.
     // We also use the name to filter the second entry point for applying code minification via UglifyJS
     entry: {
@@ -9,7 +13,7 @@ module.exports = {
     // The output defines how and where we want the bundles. The special value `[name]` in `filename` tells Webpack to use the name we defined above.
     // We target a UMD and name it vocabulary. When including the bundle in the browser it will be accessible at `window.vocabulary`
     output: {
-        path: path.resolve(__dirname, 'dist-umd'),
+        path: resolvePath('dist-umd'),
         filename: '[name].js',
         libraryTarget: 'umd',
         library: 'vocabulary',
@@ -38,3 +42,13 @@ module.exports = {
         ]
     }
 }
+
+if (process.env.NODE_WEBPACK_LIBRARY_PATH) {
+    config.output.path = resolvePath(process.env.NODE_WEBPACK_LIBRARY_PATH);
+}
+
+if (process.env.NODE_WEBPACK_LIBRARY_TARGET) {
+    config.output.libraryTarget = process.env.NODE_WEBPACK_LIBRARY_TARGET;
+}
+
+module.exports = config;
